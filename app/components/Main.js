1f80cad98a635de6e89de8874b9e035d7ee7809a
swarrier16 <swarrier16@gmail.com>
2018-05-06 22:44:58 -0400

    Add and delete functions added

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity} from 'react-native';
+import Note from './Note';
 
 export default class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            noteArray: [],
+            noteText: '',
+        }
+    }
     render() {
+
+        let notes = this.state.noteArray.map((val,key) => {
+            return <Note key={key} keyval={key} val={key}
+                   deleteMethod={ ()=> this.deleteNote(key)} />
+        });
         return (
             <View style ={styles.container}>
 
@@ -11,25 +25,48 @@ export default class App extends React.Component {
                 </View>
 
                 <ScrollView style={styles.scrollContainer}>
-
+                    {notes}
 
                 </ScrollView>
 
                 <View style={styles.footer}>
                     <TextInput
                        style={styles.textInput}
+                       onChangeText ={(noteText) => this.setState({noteText})}
+                       value={this.state.noteText}
                        placeholder="note"
                        placeholderTextColor="white"
                        underlineColorAndroid ="transparent">
                     </TextInput>
                 </View>
 
-                <TouchableOpacity style={styles.addButton}>
+                <TouchableOpacity onPress={this.addNote.bind(this)}style={styles.addButton}>
                     <Text style={styles.addButtonText}>+</Text>
                 </TouchableOpacity>
             </View>
         );
     }
+
+    addNote() {
+
+        if (this.state.noteText){
+            var d = new Date();
+            this.state.noteArray.push({
+                'date': d.getFullYear() +
+                "/" + (d.getMonth() + 1) +
+                "/" + d.getDate(),
+                'note' : this.state.noteText
+            });
+            this.setState({ noteArray: this.state.noteArray })
+            this.setState({noteText: ''});
+        }
+    }
+
+    deleteNote(key){
+        this.state.noteArray.splice(key,1);
+        this.setState({noteArray: this.state.noteArray})
+    }
+
 }
 
 const styles = StyleSheet.create({
--- app/components/Note.js
+++ app/components/Note.js
@@ -29,13 +29,20 @@ const styles = StyleSheet.create({
     noteText:{
         paddingLeft: 20,
         borderLeftWidth: 10,
-        borderLeftColor: 'E91E63',
+        borderLeftColor: '#E91E63',
     },
     noteDelete:{
         position: 'absolute',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#2980b9',
-    }
+        padding: 10,
+        top: 10,
+        bottom: 10,
+        right: 10,
+    },
+    noteDeleteText:{
+        color: 'white',
+    },
 });
 
